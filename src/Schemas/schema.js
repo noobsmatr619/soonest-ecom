463c7f6c76c19a6025da3efc26b1133f809ec0b8
Abrar Tajwar <s.tajwar@students.plymouth.ac.uk>
2020-12-16 22:18:20 +0000

    Update schema.js

--- src/Schemas/schema.js
+++ src/Schemas/schema.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    // : { type: Number, required: true, unique: true },
+    //userId : { type: Number, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 7 },
     actualName: { type: String },
