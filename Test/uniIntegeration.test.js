d19e79fc2130655f5497a1f41a04b2daf3a6705d
Abrar Tajwar <s.tajwar@students.plymouth.ac.uk>
2021-01-12 05:27:05 +0000

    merged beta and experimental tests into unitIntegration tests

--- Test/uniIntegeration.test.js
+++ Test/uniIntegeration.test.js
@@ -12,20 +12,20 @@ const testChat = require("../Model/Chat");
 
 // Starting Unit test for database
 
-describe('Mongo DB Collection tests', ()=>{
-    before( async ()=>{
+describe('Mongo DB Collection tests', () => {
+    before(async () => {
         await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useCreateIndex: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
-          });
+        });
         const con = mongoose.connection;
     });
 
-    describe('Test User', () =>{
+    describe('Test User', () => {
         // User collection test cases
-        it('USer saved to database',  (done)=>{
+        it('User saved to database', (done) => {
             let user = new testUser({
                 email: 'mytestuser@gmail.com',
                 password: '123456789',
@@ -37,53 +37,53 @@ describe('Mongo DB Collection tests', ()=>{
                 resettoken: '',
                 expiretoken: new Date()
             });
-             user.save((err, user) =>{
-                 if(err){
-                     throw new Error('User not created');
-                 }
-                 else{
-                     done();
-                 }
-             });
-        });
-
-        it('Donn\'t save User with duplicate email', (done) =>{
-            let user = new testUser({
-                email: 'mytestuser@gmail.com',
-                password: '123456789',
-                actualName: 'Test User',
-                cart: [],
-                admin: false,
-                wishlist: [],
-                orderhistory: [],
-                resettoken: '',
-                expiretoken: new Date()
-            });
-            user.save((err, user) =>{
-                if(err){
-                    return done();
+            user.save((err, user) => {
+                if (err) {
+                    throw new Error('User not created');
                 }
-                if(user){
-                    throw new Error('Email Duplicated');
-                }
-            })
-        });
-
-        it('Get User by email', (done)=>{
-            testUser.findOne({email: 'mytestuser@gmail.com'}, (err, user)=>{
-                if(err){
-                    throw err;
-                }
-                else if(!user){
-                    throw new Error('User not found');
-                }
-                else{
+                else {
                     done();
                 }
             });
         });
 
-        it('Create User with small password', (done) =>{
+        it('Don\'t save User with duplicate email', (done) => {
+            let user = new testUser({
+                email: 'mytestuser@gmail.com',
+                password: '123456789',
+                actualName: 'Test User',
+                cart: [],
+                admin: false,
+                wishlist: [],
+                orderhistory: [],
+                resettoken: '',
+                expiretoken: new Date()
+            });
+            user.save((err, user) => {
+                if (err) {
+                    return done();
+                }
+                if (user) {
+                    throw new Error('Email Duplicated');
+                }
+            })
+        });
+
+        it('Get User by email', (done) => {
+            testUser.findOne({ email: 'mytestuser@gmail.com' }, (err, user) => {
+                if (err) {
+                    throw err;
+                }
+                else if (!user) {
+                    throw new Error('User not found');
+                }
+                else {
+                    done();
+                }
+            });
+        });
+
+        it('Create User with small password', (done) => {
             let user = new testUser({
                 email: 'mytestuser1@gmail.com',
                 password: '123456',
@@ -95,17 +95,17 @@ describe('Mongo DB Collection tests', ()=>{
                 resettoken: '',
                 expiretoken: new Date()
             });
-            user.save(err =>{
-                if(err){
+            user.save(err => {
+                if (err) {
                     done();
                 }
-                else{
+                else {
                     throw new Error('User Password weak');
                 }
             })
         });
 
-        it('Try create user with wrong data type', (done) =>{
+        it('Try create user with wrong data type', (done) => {
             let user = new testUser({
                 email: 'mytestuser1@gmail.com',
                 password: '123456',
@@ -117,41 +117,42 @@ describe('Mongo DB Collection tests', ()=>{
                 resettoken: '',
                 expiretoken: new Date()
             });
-            user.save(err =>{
-                if(err){
+            user.save(err => {
+                if (err) {
                     return done();
                 }
-                else{
+                else {
                     throw new Error('User Password weak');
                 }
             })
         });
     });
 
-    it('Try creating chat for user',  (done) =>{
+    it('Try creating chat for user', (done) => {
         let chat = new testChat({});
-        chat.save(err =>{
-            if(err){
+        chat.save(err => {
+            if (err) {
                 throw new Error('Chat not created');
             }
-            else{
+            else {
                 done();
             }
         });
     });
 
-    
-    after((done)=>{
-        testUser.findOneAndRemove({email: 'mytestuser@gmail.com'}, (err, user)=>{
-            if(err){
+
+    after((done) => {
+        testUser.findOneAndRemove({ email: 'mytestuser@gmail.com' }, (err, user) => {
+            if (err) {
                 throw new Error('User not deleted');
             }
-            else{
+            else {
                 done();
             }
         })
     });
 });
+
 
 // Starting integeration Test
 
@@ -160,7 +161,7 @@ chai.should();
 chai.use(chaiHttp);
 
 
-describe('Routes Testing', ()=>{
+describe('Routes Testing', () => {
 
     describe('Testing', () => {
         it('it should create a new user', (done) => {
@@ -187,23 +188,125 @@ describe('Routes Testing', ()=>{
 
         }).timeout(10000);
 
+
+        describe('Add an admin', () => {
+            it('it should add an admin', (done) => {
+                chai.request(server)
+                    .post('/api/admin/addadmin')
+                    .set('Content-Type', 'application/json')
+
+                    .send(testData.addAdmin)
+                    .end((err, response) => {
+                        response.should.have.status(200);
+                        done();
+                    })
+
+            }).timeout(10000);
+
+        });
+        describe('login an admin', () => {
+            it('it should login', (done) => {
+                chai.request(server)
+                    .post('/api/admin/login')
+                    .set('Content-Type', 'application/json')
+
+                    .send(testData.addAdmin)
+                    .end((err, response) => {
+                        // console.log(response);
+                        response.should.have.status(200);
+                        done();
+                    })
+
+            }).timeout(10000);
+
+        });
+        describe('it should get admin information', () => {
+            it('get admin information sucessfullt', (done) => {
+                chai.request(server)
+                    .get('/api/admin/')
+                    .set('Content-Type', 'application/json')
+                    .set('x-auth-token', testData.AdminToken)
+                    .end((err, response) => {
+                        response.should.have.status(200);
+                        done();
+                    })
+
+            }).timeout(10000);
+
+        });
+
+
+        describe('it should get the product list', () => {
+            it('list all products sucessfully', (done) => {
+                chai.request(server)
+                    .get('/api/product/')
+                    .set('Content-Type', 'application/json')
+                    .end((err, response) => {
+                        expect(response.body).to.be.an('Array');
+                        done();
+                    })
+
+            }).timeout(10000);
+
+        });
+        describe('list all users', () => {
+            it('it should get the list of users', (done) => {
+                chai.request(server)
+                    .get(`/api/user/all`)
+                    .set('Content-Type', 'application/json')
+                    .set('x-auth-token', testData.AdminToken)
+                    .end((err, response) => {
+                        expect(response.body).to.be.an('Array')
+                        done();
+                    });
+            }).timeout(10000);
+
+        });
+
+        describe('Get all Blogs', () => {
+            it('it should list of all blog', (done) => {
+                chai.request(server)
+                    .get(`/api/blog/`)
+                    .set('Content-Type', 'application/json')
+                    .end((err, response) => {
+                        expect(response.body).to.be.an('Array');
+                        done();
+                    });
+            }).timeout(10000);
+
+        });
         it('user deletion without authorization', (done) => {
             chai.request(server)
                 .delete('/api/user/delete')
                 .set('Content-Type', 'application/json')
-                .send({email: testData.createNewUser.email})
+                .send({ email: testData.createNewUser.email })
                 .end((err, response) => {
                     response.should.have.status(401);
                     done();
                 });
         }).timeout(10000);
 
+
         it('user deletion with authorization', (done) => {
             chai.request(server)
                 .delete('/api/user/delete')
                 .set('Content-Type', 'application/json')
-                .set('x-auth-token',testData.Token)
-                .send({email: testData.createNewUser.email})
+                .set('x-auth-token', testData.Token)
+                .send({ email: testData.createNewUser.email })
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    done();
+                })
+
+        }).timeout(10000);
+
+       
+        it('Admin deletion with authorization', (done) => {
+            chai.request(server)
+                .delete('/api/user/delete')
+                .set('Content-Type', 'application/json')
+                .set('x-auth-token', testData.Token)
+                .send({ email: testData.addAdmin.email })
                 .end((err, response) => {
                     response.should.have.status(200);
                     done();
@@ -215,8 +318,8 @@ describe('Routes Testing', ()=>{
             chai.request(server)
                 .delete('/api/user/delete')
                 .set('Content-Type', 'application/json')
-                .set('x-auth-token',testData.Token)
-                .send({email: 'randomemailtest22@test.com'})
+                .set('x-auth-token', testData.Token)
+                .send({ email: 'randomemailtest22@test.com' })
                 .end((err, response) => {
                     response.should.have.status(200);
                     done();
@@ -226,7 +329,7 @@ describe('Routes Testing', ()=>{
 
     });
 
-    describe('Testing routes validation', () =>{
+    describe('Testing routes validation', () => {
 
         it('It should not fetch products', (done) => {
             chai.request(server)
@@ -253,7 +356,7 @@ describe('Routes Testing', ()=>{
             chai.request(server)
                 .get(`/api/product/${testData.listOneProduct}`)
                 .set('Content-Type', 'application/json')
-                .set('token',testData.AdminToken)
+                .set('token', testData.AdminToken)
                 .end((err, response) => {
                     response.should.have.status(401);
                     done();
@@ -261,7 +364,7 @@ describe('Routes Testing', ()=>{
 
         }).timeout(10000);
 
-        it('It should close database connection', (done)=>{
+        it('It should close database connection', (done) => {
             mongoose.connection.close();
             done();
         });
@@ -271,7 +374,7 @@ describe('Routes Testing', ()=>{
                 .get('/api/product/')
                 .set('Content-Type', 'application/json')
                 .timeout(4000)
-                .catch(err =>{
+                .catch(err => {
                     done();
                 })
 
